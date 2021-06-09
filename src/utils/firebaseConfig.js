834b4a0de92c09f18f969b28a6c4b38f33ff1df7
corujasdevbr <fernando.guerra@corujasdev.com.br>
2021-06-09 15:58:37 -0300

    config firebase

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 // import admin from 'firebase-admin';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyABB99ncRdO_2r0BUZchWeQJvVJemLjPQw",
-    authDomain: "chatbotlinx-5c927.firebaseapp.com",
-    databaseURL: "https://chatbotlinx-5c927-default-rtdb.firebaseio.com",
-    projectId: "chatbotlinx-5c927",
-    storageBucket: "chatbotlinx-5c927.appspot.com",
-    messagingSenderId: "992298893503",
-    appId: "1:992298893503:web:bee10c2e45f7926096860a"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = firebase.initializeApp(firebaseConfig);
